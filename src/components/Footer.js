39a556e82c341b347ef6806c3ba5dd0f310e1648
Carlo Berardelli <carloberardelli@yahoo.com>
2021-01-30 13:11:03 -0500

    added header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from  "react"
+
+function Footer() {
+    return (
+      <div className="Footer">
+        
+          <p>
+            hii I'm the footer
+          </p>
+          
+      </div>
+    );
+  }
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from  "react"
+
+function Header() {
+    return (
+      <div className="Header">
+        
+          <p>
+            sup yall I'm the header
+          </p>
+          
+      </div>
+    );
+  }
+
+export default Header
