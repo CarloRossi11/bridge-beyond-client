f6c798c8cf7705c0c16944da006999db85e4ac46
Carlo Berardelli <carloberardelli@yahoo.com>
2021-02-04 19:04:33 -0500

    made successfull api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [resource, setResource] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(async () => {
+    const response = await fetch('https://buddy-system-api.herokuapp.com/resources')
+    const data = await response.json()
+    const [item] = data
+    setResource(item)
+    setLoading(false)
+  }, [])
+
   return (
     <div className="App">
       <Header/>
       <div >
-        <p>Bridge Beyond Stuff</p>
+        <h1>Bridge Beyond Buddy</h1>
+
+        {loading ? <div>...Loading</div> : <div>{resource.name}</div>}
+
       </div>
       <Footer/>
     </div>
