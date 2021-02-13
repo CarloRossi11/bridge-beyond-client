d8844fc91240ba55990a406ee0265fb1a3fbfc12
Carlo Berardelli <carloberardelli@yahoo.com>
2021-02-13 13:55:26 -0500

    added search functionality

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import {getList} from './components/List'
 function App() {
   const [loading, setLoading] = useState(true)
   const [list, setList] = useState([]);
+  const [search, setSearch] = useState('')
 
+  //Fetch Logic
   useEffect(() => {
     let mounted = true;
     getList()
@@ -20,14 +22,22 @@ function App() {
     return () => mounted = false;
   }, [])
 
+  //Search Logic
+  const filteredList = list.filter( resource => {
+    return resource.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <div className="App">
       <Header/>
+
       <div >
         <h1>Bridge Beyond Buddy</h1>
 
+        <input type="text" placeholder="Search" onChange={e =>setSearch(e.target.value) }/>
+
         <ul>
-          {loading ? <div>...Loading</div> : list.map(resource =>
+          {loading ? <div>...Loading</div> : filteredList.map(resource =>
             <div> 
               <h2>{resource.name}</h2>
               <h4>{resource.description}</h4>
@@ -36,9 +46,8 @@ function App() {
             )}
         </ul>
 
-        {/* {loading ? <div>...Loading</div> : <div>{resource.name}</div>} */}
-
       </div>
+
       <Footer/>
     </div>
   );
