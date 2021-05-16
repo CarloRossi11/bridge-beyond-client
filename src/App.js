ffe2fb8be540e52a69f0af05a8733f7fbabc2c56
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2021-05-15 23:11:27 -0600

    multiSeclect added and some styling changed

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import Checkbox from './components/Checkbox'
 import Dropdown from './components/Dropdown'
 import { getList } from './components/List'
 import { getCats } from './components/CatCall'
+import MultiSelect from "react-multi-select-component";
+
 
 function App() {
   const [loading, setLoading] = useState(false)
   const [list, setList] = useState([]);
   const [cats, setCats] = useState([]);
   const [checked, setChecked] = useState(true);
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('');
+  const [selected, setSelected] = useState([]);
 
+  const options = [];
 //Fetch Resource Logic
   useEffect(() => {
     let mounted = true;
@@ -41,17 +45,14 @@ function App() {
 
 //Search Logic
   const filteredList = list.filter( resource => {
+    // todo : apply filters here based on what is set
+    //resource.categories.where(x=>x.name == 'what is in your filtered set a , delimited list of category names')
     return resource.name.toLowerCase().includes(search.toLowerCase())
   })
 
-//Checkbox Logic
-  const handleCheckbox = (checked) => {
-    setChecked(checked)
-    if (Checkbox.selected === {checked}){
-      //if the category.name checkbox is selected, then display resource ul below
-      // -else-
-      // set resource to display:"none"
-    }
+  const setCategories = (listOf)=>{
+    setSelected(listOf);
+    console.log("todo: igure out how to fire off the filteredList logic having it consider the selected categories")
   }
 
   return (
@@ -62,31 +63,43 @@ function App() {
         <h1>Bridge Beyond Outreach</h1>
 
         <div className="search">
-          <p className="searchtext">Search by Resource Name</p>
+          <h4 className="searchtext">Search by Resource Name</h4>
           <input type="text" placeholder="Resource Name" onChange={e =>setSearch(e.target.value)}/>
         </div>
-
-        <Dropdown placeholder='Categories'>
+        <div>
+    </div>
+        {/* map data to options array */}
         {cats.map(category => 
-            <Checkbox styleClass="catchecks" label={category.name} onChange={handleCheckbox} selected={checked}/>
-          )}
-        </Dropdown>
-
+            options.push({label:category.name, value:category.name})
+        )}
+        <div className="multiSelect">
+          <h4>Search by Category</h4>
+        <MultiSelect
+        options={options}
+        value={selected}
+        onChange={setCategories}
+        labelledBy="Select a Category"
+        disableSearch={true}
+      />
+      </div>
+     
         <ul>
           {loading ? filteredList.map(resource =>
-            <div className="resources" key={resource.id}>
+            <div className="resourcesCard" key={resource.id}>
+              <div className="container">
               <h2>{resource.name}</h2>
               <p>{resource.description}</p>
               <a id="bridge" href={resource.link} target="blank">Visit {resource.name} Website</a>
               <p>{resource.contact_information}</p>
               {/* category info */}
               {resource.categories.map(category => <div>{category.name}</div>)}
+              </div>
             </div> 
             ) : <Loader/>
             }
         </ul>
-
       </div>
+
       <Footer/>
     </div>
   );
